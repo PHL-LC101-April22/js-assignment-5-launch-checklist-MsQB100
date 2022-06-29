9c29cf95176dbba54b07d42d8843ac753bb64d3f
Naquessa Boddie <Naquessa.Boddie@arcosa.com>
2022-06-29 09:19:30 -0500

    Added code

--- script.js
+++ script.js
@@ -11,6 +11,19 @@ window.addEventListener("load", function() {
    }).then(function () {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
+       let planet = pickPlanet(listedPlanets);
+       addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image)
+   });
+  
+
+   let pilotName = document.querySelector("input[name=pilotName]");
+   let copilotName = document.querySelector("input[name=copilotName]");
+   let fuelLevel = document.querySelector("input[name=fuelLevel]");
+   let cargoMass = document.querySelector("input[name=cargoMass]");
+
+
+   formSubmission(document, pilotName, copilotName, fuelLevel, cargoMass);
    
+   
+
 });
